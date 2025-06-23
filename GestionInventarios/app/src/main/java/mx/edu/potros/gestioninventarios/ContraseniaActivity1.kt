92473fe4c7e69cf7efcb088249acc21fd15ab961
martinyocupicio228307 <martin.yocupicio228307@potros.itson.edu.mx>
2025-06-22 20:48:12 -0700

    Nombre de los ContraseñaActivity cambiados a "ContraseniaActivity"

--- GestionInventarios/app/src/main/java/mx/edu/potros/gestioninventarios/ContraseniaActivity1.kt
+++ GestionInventarios/app/src/main/java/mx/edu/potros/gestioninventarios/ContraseniaActivity1.kt
@@ -1,4 +1,4 @@
 package mx.edu.potros.gestioninventarios
 
-class ContraseñaActivity2 {
+class ContraseniaActivity1 {
 }
--- GestionInventarios/app/src/main/java/mx/edu/potros/gestioninventarios/ContraseniaActivity2.kt
+++ GestionInventarios/app/src/main/java/mx/edu/potros/gestioninventarios/ContraseniaActivity2.kt
@@ -1,4 +1,4 @@
 package mx.edu.potros.gestioninventarios
 
-class ContraseñaActivity1 {
+class ContraseniaActivity2 {
 }
