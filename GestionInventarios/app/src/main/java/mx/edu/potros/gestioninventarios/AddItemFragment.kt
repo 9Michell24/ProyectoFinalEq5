2499317d69aea39fb94b17c0d1e7824142c235f0
michelle <michellebojorquezsterling@gmail.com>
2025-06-28 18:10:44 -0700

    se corrige registrar artículo

--- GestionInventarios/app/src/main/java/mx/edu/potros/gestioninventarios/AddItemFragment.kt
+++ GestionInventarios/app/src/main/java/mx/edu/potros/gestioninventarios/AddItemFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class AddItemFragment : Fragment() {
 
@@ -27,4 +31,31 @@ class AddItemFragment : Fragment() {
     ): View {
         return inflater.inflate(R.layout.fragment_add_item2, container, false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    // Spinner
+        val spinnerCategoríaArtículo = view.findViewById<Spinner>(R.id.spinner)
+    val adapter = ArrayAdapter.createFromResource(
+        requireContext(),
+        R.array.opciones_spinner,
+        android.R.layout.simple_spinner_item
+    )
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCategoríaArtículo.adapter = adapter
+
+        spinnerCategoríaArtículo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(
+            parent: AdapterView<*>, view: View?, position: Int, id: Long
+        ) {
+            if (position != 0) {
+                val opcionSeleccionada = parent.getItemAtPosition(position).toString()
+                Toast.makeText(requireContext(), "Seleccionaste: $opcionSeleccionada", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>) {}
+    }
+}
 }
