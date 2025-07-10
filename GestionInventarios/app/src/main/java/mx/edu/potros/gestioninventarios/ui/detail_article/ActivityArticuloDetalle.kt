76b402a77b0cf839ab3c3538b1121566ea8420b3
M4rtinYocupicio <martin.yocupicio228307@potros.itson.edu.mx>
2025-07-09 22:21:29 -0700

    Se restauró el nombre de ActivityArticuloDetalle

--- GestionInventarios/app/src/main/java/mx/edu/potros/gestioninventarios/ui/detail_article/ActivityArticuloDetalle.kt
+++ GestionInventarios/app/src/main/java/mx/edu/potros/gestioninventarios/ui/detail_article/ActivityArticuloDetalle.kt
@@ -2,9 +2,7 @@ package mx.edu.potros.gestioninventarios.ui.detail_article
 
 import android.app.AlertDialog
 import android.graphics.Color
-import android.graphics.Typeface
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.*
 import android.widget.*
